1eb2e16f7a38e9b0dcbfb52a93a230787aa89245
zorionzhou <zorionzhou@163.com>
2019-02-16 23:32:33 +0800

    修改配置文件

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import {app, BrowserWindow, Menu} from 'electron'
 
 /**
  * Set `__static` path to static files in production
@@ -24,7 +24,8 @@ function createWindow () {
     useContentSize: true,
     width: 1000
   })
-
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
   mainWindow.loadURL(winURL)
 
   mainWindow.on('closed', () => {
@@ -45,3 +46,88 @@ app.on('activate', () => {
     createWindow()
   }
 })
+let template = [{
+  label: 'RedisPro',
+  submenu: [{
+    label: '撤销',
+    accelerator: 'CmdOrCtrl+Z',
+    role: 'undo'
+  }, {
+    label: '重做',
+    accelerator: 'Shift+CmdOrCtrl+Z',
+    role: 'redo'
+  }, {
+    type: 'separator'
+  }, {
+    label: '剪切',
+    accelerator: 'CmdOrCtrl+X',
+    role: 'cut'
+  }, {
+    label: '复制',
+    accelerator: 'CmdOrCtrl+C',
+    role: 'copy'
+  }, {
+    label: '粘贴',
+    accelerator: 'CmdOrCtrl+V',
+    role: 'paste'
+  }, {
+    label: '全选',
+    accelerator: 'CmdOrCtrl+A',
+    role: 'selectall'
+  }]
+}, {
+  label: '查看',
+  submenu: [{
+    label: '切换全屏',
+    accelerator: (() => {
+      if (process.platform === 'darwin') {
+        return 'Ctrl+Command+F'
+      } else {
+        return 'F11'
+      }
+    })(),
+    click: (item, focusedWindow) => {
+      if (focusedWindow) {
+        focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+      }
+    }
+  }, {
+    label: '切换开发者工具',
+    accelerator: (() => {
+      if (process.platform === 'darwin') {
+        return 'Alt+Command+I'
+      } else {
+        return 'Ctrl+Shift+I'
+      }
+    })(),
+    click: (item, focusedWindow) => {
+      if (focusedWindow) {
+        focusedWindow.toggleDevTools()
+      }
+    }
+  }, {
+    type: 'separator'
+  }]
+}, {
+  label: '窗口',
+  role: 'window',
+  submenu: [{
+    label: '最小化',
+    accelerator: 'CmdOrCtrl+M',
+    role: 'minimize'
+  }, {
+    label: '关闭',
+    accelerator: 'CmdOrCtrl+W',
+    role: 'close'
+  }, {
+    type: 'separator'
+  }, {
+    label: '重新打开窗口',
+    accelerator: 'CmdOrCtrl+Shift+T',
+    enabled: false,
+    key: 'reopenMenuItem',
+    click: () => {
+      app.emit('activate')
+    }
+  }]
+}]
