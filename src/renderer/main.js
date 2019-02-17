6ca583997c001db88620a88f729fbd33f0ae3b92
zorionzhou <zorionzhou@163.com>
2019-02-17 23:26:41 +0800

    修改标题栏

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import i18n from './lang'
-
+import 'element-ui/lib/theme-chalk/index.css'
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
