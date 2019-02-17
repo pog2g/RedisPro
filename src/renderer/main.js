cd37dd89c377a1f2a1080e38f847790677e2a93d
zorionzhou <zorionzhou@163.com>
2019-02-17 22:59:47 +0800

    修改标题栏

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,18 +1,23 @@
 import Vue from 'vue'
 import axios from 'axios'
-
+import Element from 'element-ui'
 import App from './App'
 import router from './router'
 import store from './store'
+import i18n from './lang'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
+Vue.use(Element, {
+  size: 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
 /* eslint-disable no-new */
 new Vue({
-  components: { App },
+  components: {App},
   router,
   store,
+  i18n,
   template: '<App/>'
 }).$mount('#app')
