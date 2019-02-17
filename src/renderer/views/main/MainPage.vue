1dff51ae0d53b15516ccbe67c244026850c539df
zorionzhou <zorionzhou@163.com>
2019-02-18 02:04:17 +0800

    修改标题栏

--- src/renderer/views/main/MainPage.vue
+++ src/renderer/views/main/MainPage.vue
@@ -1,28 +1,13 @@
 <template>
     <div id="wrapper">
-        <main>
-            <div class="demo-split">
-                <Split v-model="split1">
-                    <div slot="left" class="demo-split-pane">
-                        Left Pane
-                    </div>
-                    <Divider type="vertical"/>
-                    <div slot="right" class="demo-split-pane">
-                        Right Pane
-                    </div>
-                </Split>
-            </div>
-        </main>
+        lsdkfjsldfjsdlkfj
     </div>
 </template>
-
 <script>
   export default {
     name: 'main-page',
     data () {
-      return {
-        split1: 0.3
-      }
+      return {}
     }
   }
 </script>
@@ -47,25 +32,7 @@
                 rgba(229, 229, 229, .9) 100%
         );
         height: 100vh;
-        padding: 70px 0 0 0;
+        padding: 70px 2px 2px 2px;
         width: 100vw;
-    }
-
-    main {
-        display: flex;
-        justify-content: space-between;
-    }
-
-    main > div {
-        flex-basis: 50%;
-    }
-
-    .demo-split {
-        height: 93vh;
-        border: 1px solid #dcdee2;
-    }
-
-    .demo-split-pane {
-        padding: 1px;
     }
 </style>
